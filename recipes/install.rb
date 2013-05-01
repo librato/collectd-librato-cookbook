ceefe97c89ab2ec9a29414b985071a5e4b3cfe76
Lloyd Philbrook <lloyd@tablexi.com>
2013-05-01 21:36:45 +0800

    using new LWRP resource

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,16 +1,18 @@
 include_recipe 'collectd::client'
 ver = node[:collectd_librato][:version]
 
-# Install plugin
-collectd_python_plugin collectd-librato do
-  path "/opt/collectd-librato-#{ver}/lib"
-  opts = {
-    'APIToken' => node[:collectd_librato][:api_token],
-    'Email' => node[:collectd_librato][:email]
-  }
+opts = {
+  'APIToken' => node[:collectd_librato][:api_token],
+  'Email' => node[:collectd_librato][:email]
+}
 
-  if node[:collectd_librato][:api]
-    opts['Api'] = node[:collectd_librato][:api]
-  end
+if node[:collectd_librato][:api]
+  opts['Api'] = node[:collectd_librato][:api]
+end
+
+# Install plugin
+collectd_plugin 'collectd-librato' do
+  path "/opt/collectd-librato-#{ver}/lib"
+  type 'python'
   options(opts)
 end
