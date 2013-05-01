52340f395386bca5dffc8ad3c852f44d3652fc13
Lloyd Philbrook <lloyd@tablexi.com>
2013-05-01 21:39:57 +0800

    fixed another bug

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,6 +4,7 @@ ver = node[:collectd_librato][:version]
 opts = {
   'APIToken' => node[:collectd_librato][:api_token],
   'Email' => node[:collectd_librato][:email]
+  'paths' => ["/opt/collectd-librato-#{ver}/lib"]
 }
 
 if node[:collectd_librato][:api]
@@ -12,7 +13,6 @@ end
 
 # Install plugin
 collectd_plugin 'collectd-librato' do
-  path "/opt/collectd-librato-#{ver}/lib"
   type 'python'
   options(opts)
 end
